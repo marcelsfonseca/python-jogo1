132e98a7a9cc367e08232a562f9c16a66e8ad2ff
Marcel S Fonseca <marcelsfonseca@gmail.com>
2022-10-09 23:12:26 -0300

    v0.2 - Comentando a função int()

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ numero_secreto = 42
 
 chute_str = input("Digite o seu número: ")
 print("Você digitou: ", chute_str)
+#Convertendo o tipo str para o tipo int/Todo dado obtido pelo input é do tipo str
 chute = int(chute_str)
 
 if (numero_secreto == chute):
